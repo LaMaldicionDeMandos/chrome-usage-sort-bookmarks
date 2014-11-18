d29208587197a342c9221f00b4a64901ab59f1dd
Marcelo Pasut <pasutmarcelo@gmail.com>
2014-11-17 22:18:31 -0300

    agrego funciones base para procesar los bookmarks

--- background.js
+++ background.js
@@ -1,7 +1,18 @@
+var processTree = function(tree) {
+	console.log('Bookmarks: ' + tree.id + ' ' + tree.index + ' ' + tree.title);
+};
+
+var acumulateTrees = function(trees) {
+	for (i = 0; i< trees.length; i++) {
+  		processTree(trees[i]);
+  		chrome.bookmarks.getChildren(trees[i].id, acumulateTrees);
+  	}
+};
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status == 'loading') {
   	console.log('tabs.onUpdated -- window: ' + tab.windowId + ' tab: ' + tab.id +
       ' title: ' + tab.title + ' index ' + tab.index + ' url ' + tab.url);  	
+  	chrome.bookmarks.getTree(acumulateTrees);
   }
-
 });
