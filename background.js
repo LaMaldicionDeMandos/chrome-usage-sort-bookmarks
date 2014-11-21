a2aed2a0c30433ea08e345b3f57f988f5052ec70
Marcelo Pasut <pasutmarcelo@gmail.com>
2014-11-21 00:03:30 -0300

    aplico pequeños cambios, parece que ya anda bien

--- background.js
+++ background.js
@@ -1,18 +1,77 @@
+function Bookmark(bookmarkNode, counts) {
+  this.id = bookmarkNode.id;
+  this.parentId = bookmarkNode.parentId;
+  this.index = bookmarkNode.index;
+  this.name = bookmarkNode.title;
+  this.url = bookmarkNode.url;
+  this.counts = counts || Bookmark.createEmptyCounts();
+  this.completeCounts = function() {
+    var date = new Date();
+    var month = date.getMonth();
+    var year = date.getFullYear();
+    var time = year*12 + month;
+    var counts = this.counts || Bookmark.createEmptyCounts();
+    var countsTime = counts[0].year*12 + counts[0].month;
+    while(time > countsTime++) {
+      this.counts.unshift(Bookmark.createEmptyCount());
+    }
+    this.counts = counts;
+  };
+
+  this.count = function() {
+    this.completeCounts(this.counts);
+    var ret = 0;
+    for (i = 12;i>0;i--) {
+      ret+= this.counts[12 - i] != undefined ? this.counts[12 - i].count*i : 0;
+    }
+    return ret;
+  };
+
+  this.use = function(callback) {
+    var date = new Date();
+    var lastCounts = this.counts[0];
+    if (date.getMonth() != lastCounts.month || date.getFullYear() != lastCounts.year) {
+      lastCounts = Bookmark.createEmptyCount();
+      this.counts.unshift(lastCounts);
+    }
+    lastCounts.count++;
+    var dto = {};
+    var self = this;
+    dto[this.url] = this.counts;
+    chrome.storage.sync.set(dto, function() {
+      callback(self);
+    });
+  };
+
+  this.move = function(index) {
+    if ( this.index != index) {
+      chrome.bookmarks.move(this.id, {'index':index});
+      console.log("Change bookmark: " + this.name + " index: " + index);
+      this.index = index;
+    }
+  };
+};
+
+Bookmark.createEmptyCount = function() {
+  var date = new Date();
+  return {month: date.getMonth(), year: date.getFullYear(), count: 0};
+};
+
+Bookmark.createEmptyCounts = function() {
+  return [Bookmark.createEmptyCount()];
+};
+
 var bookmarks = {};
 var processTree = function(tree) {
-	console.log('Bookmarks: ' + tree.id + ' ' + tree.index + ' ' + tree.title);
-  var bookmark = {};
-  bookmark.id = tree.id;
-  bookmark.parentId = tree.parentId;
-  bookmark.index = tree.index;
-  bookmark.name = tree.title;
-  bookmark.url = tree.url;
-  bookmark.count = 0;
-  bookmarks[tree.url] = bookmark;
-  chrome.storage.sync.get(tree.url, function(count) {
-    bookmark.count = count[tree.url] | 0;
-    console.log("bookmark: {url: " + bookmark.url + ", count: " + bookmark.count + "}");
-  });
+	console.log('Bookmarks: ' + tree.id + ' ' + tree.index + ' ' + tree.title + " url: " + tree.url);
+  if (tree.url) {
+    var bookmark = new Bookmark(tree);
+    bookmarks[tree.url] = bookmark;
+    chrome.storage.sync.get(tree.url, function(values) {
+      bookmark.counts = values != undefined ? values[tree.url] || Bookmark.createEmptyCounts() : Bookmark.createEmptyCounts();
+      console.log("bookmark: {url: " + bookmark.url + ", count: " + bookmark.count() + "}");
+    });
+  }
 };
 
 var acumulateTrees = function(trees) {
@@ -40,39 +99,38 @@ var findChildren = function(parentId) {
   return children;
 };
 
-chrome.windows.onCreated.addListener(function() {
+var calculateBookmarkPosition = function(bookmark) {
+  var books = findChildren(bookmark.parentId);
+  books.sort(function(a, b) {
+    if (a.count() > b.count()) return -1;
+    if (a.count() < b.count()) return 1;
+    return (a.index <= b.index) ? -1 : 1;
+  });
+  for (var index = 0; index < books.length;index++) {
+    if (books[index].id == bookmark.id) {
+      bookmark.move(index);
+    }
+    books[index].index = index;
+    console.log("Bookmark: " + books[index].name + " -- Index: " + books[index].index + " -- Points: " + books[index].count());
+  }
+};
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.bookmarks.getTree(initBookmarks);
+});
+
+chrome.runtime.onStartup.addListener(function() {
   chrome.bookmarks.getTree(initBookmarks);
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  console.log("Change tab: {status: " + changeInfo.status + ", url: " + changeInfo.url);
   if (changeInfo.status == 'loading' && changeInfo.url != undefined) {
     var url = changeInfo.url;
     console.log("Finding: " + url)
     bookmark = findBookmarkByUrl(url);
     if (bookmark != undefined) {
       console.log("Found bookmark: " + bookmark.name);
-      bookmark.count++;
-      var dto = {};
-      dto[bookmark.url] = bookmark.count;
-      chrome.storage.sync.set(dto, function() {
-        console.log("New bookmark count: " + bookmark.count);
-        var books = findChildren(bookmark.parentId);
-        books.sort(function(a, b) {
-          if (a.count > b.count) return -1;
-          if (a.count < b.count) return 1;
-          return (a.index <= b.index) ? -1 : 1;
-        });
-        for (var index = 0; index < books.length;index++) {
-          if (books[index].id == bookmark.id && index != bookmark.index) {
-            chrome.bookmarks.move(bookmark.id, {'index':index});
-            console.log("Change bookmark: " + bookmark.name + " index: " + index);
-            books[index].index = index;
-            return;
-          }
-        }
-      });
+      bookmark.use(calculateBookmarkPosition);
     }
   }
 });
-
